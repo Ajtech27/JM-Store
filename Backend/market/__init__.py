61beac10ce566b8cf83e04c989ddc01f2ac9ace4
Ajtech <ajtech012@gmail.com>
2026-08-28 15:30:09 +0100

    key fix

--- Backend/market/__init__.py
+++ Backend/market/__init__.py
@@ -18,6 +18,7 @@ def create_app():
         database_url = database_url.replace('postgres://', 'postgresql://', 1)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    app.config['SECRET_KEY'] = '9f02f163fb48822d3cc8d603'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     #CORS(app, origins=['https://your-frontend.vercel.app'], supports_credentials=True)
